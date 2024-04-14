e1eee72ec003fa56c6d620053b1400e667254e4b
Iskandar <iskandarmuminov@mail.ru>
2024-04-14 22:44:05 +1000

    Implemented FOV displaying objects only in FOV

---
+++ Assets/Script/SceneViewMask.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneViewMask : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
