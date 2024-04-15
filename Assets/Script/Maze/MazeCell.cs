356c4695788a6e761c55d52bf1d3d133e76eac9e
Belida-Ouch <belida.ouch@gmail.com>
2024-04-16 01:55:33 +1000

    New tiles and generation

--- Assets/Script/Maze/MazeCell.cs
+++ Assets/Script/Maze/MazeCell.cs
@@ -1,58 +1,52 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MazeCell : MonoBehaviour
 {
-    [SerializeField] BoxCollider mazeCellCollider;
-    [SerializeField] Transform mazeTransform;
-    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] private float rotationSpeed = 100f;
     private int rotationAngle = 90;
     private bool playerInside = false;
     private bool isRotating = false;
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
-        if (playerInside && !isRotating && Input.GetKeyDown(KeyCode.E)) 
+        if (playerInside && !isRotating && Input.GetKeyDown(KeyCode.E))
         {
-            StartCoroutine(RotateCell()); 
+            StartCoroutine(RotateCell());
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) 
+        
+        if (other.CompareTag("Player"))
         {
-            playerInside = true; 
+            playerInside = true;
+            Debug.Log("Entered");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
-            playerInside = false; 
+            playerInside = false;
         }
     }
 
-    IEnumerator RotateCell()
+     IEnumerator RotateCell()
     {
         isRotating = true; 
 
-        Quaternion startRotation = mazeTransform.rotation;
-        Quaternion targetRotation = mazeTransform.rotation * Quaternion.Euler(0, rotationAngle, 0); 
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, rotationAngle, 0); 
 
         float t = 0.0f; 
 
         while (t < 1.0f)
         {
             t += Time.deltaTime * rotationSpeed / 90.0f; 
-            mazeTransform.rotation = Quaternion.Slerp(startRotation, targetRotation, t); 
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, t); 
             yield return null;
         }
 
