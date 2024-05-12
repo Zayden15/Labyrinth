0186b5de600c0259f5dc4e71da3c10cbe3d30630
Iskandar <iskandarmuminov@mail.ru>
2024-05-12 20:45:02 +1000

    Revert "Removed Maze stuffs"
    This reverts commit 381137f41d611042c0f4f3f1711513e70c109c97.

---
+++ Assets/Script/Maze/MazeCell.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 100f;
+    private bool playerInside = false;
+    private bool isRotating = false;
+
+
+    private void Start()
+    {
+        ActivateRotationPlatform();
+    }
+
+    void Update()
+    {
+        if (playerInside && !isRotating && Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(RotateCellLeft());
+        }
+        else if (playerInside && !isRotating && Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(RotateCellRight());
+        }
+    }
+
+    private void ActivateRotationPlatform() {
+        GameObject ChildGameObject1 = this.transform.GetChild(0).gameObject;
+        ChildGameObject1.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+            Debug.Log("Entered");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    IEnumerator RotateCellLeft()
+    {
+        int rotationAngle = 90;
+        isRotating = true;
+
+        Quaternion startRotation = transform.localRotation;
+        Quaternion targetRotation = transform.localRotation * Quaternion.Euler(0, rotationAngle, 0);
+        float t = 0.0f;
+
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * rotationSpeed / 90.0f;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, t);
+            yield return null;
+        }
+
+        isRotating = false;
+    }
+
+    IEnumerator RotateCellRight()
+    {
+        int rotationAngle = -90;
+        isRotating = true;
+
+        Quaternion startRotation = transform.localRotation;
+        Quaternion targetRotation = transform.localRotation * Quaternion.Euler(0, rotationAngle, 0);
+        float t = 0.0f;
+
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * rotationSpeed / 90.0f;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, t);
+            yield return null;
+        }
+
+        isRotating = false;
+    }
+
+}
