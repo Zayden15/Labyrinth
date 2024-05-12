91d7dd4d9afb7d185e8ea1d7f998c3ba20bba335
Belida-Ouch <belida.ouch@gmail.com>
2024-05-12 22:59:57 +1000

    Pentogram fixed

--- Assets/Pentogram.cs
+++ Assets/Pentogram.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class Pentogram : MonoBehaviour
 {
     [SerializeField] private GameObject summon;
+    private SphereCollider collider;
+
+    private void Start()
+    {
+        collider = gameObject.GetComponent<SphereCollider>();
+        if (collider == null)
+        {
+            Debug.LogError("SphereCollider not found on the GameObject!");
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && Collectible.numCollected != 0)
+        if (other.CompareTag("Player") && Collectible.numCollected > 0)
         {
             Debug.Log("Summoning");
             Collectible.numCollected--;
+            collider.enabled = false;
             summon.SetActive(true);
         }
     }
 }
+
